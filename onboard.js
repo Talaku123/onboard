133e7cd1b202babd573f487a7a917625b9e91989
Talaku123 <69880305+Talaku123@users.noreply.github.com>
2024-03-09 20:21:42 -0500

    Add files via upload

---
+++ onboard.js
@@ -0,0 +1,15 @@
+
+
+function showSidebar(){
+    
+  const sidebar = document.getElementById("js-slidebar")
+
+  sidebar.style.display = "flex"
+}
+
+function hiddenSidebar(){
+  
+const hidden = document.getElementById("js-slidebar")
+
+hidden.style.display = "none"
+}
